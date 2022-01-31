64ad953b18f1d792ddd81f376d8dfaff85c9f8bc
Viruuus30 <71880004+Viruuus30@users.noreply.github.com>
2022-01-31 10:16:16 +0100

    main

--- frontend-react/src/Components/Profil/Profil.js
+++ frontend-react/src/Components/Profil/Profil.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default () => {
     const [username, setUsername] = useState('');
     const [pwd, setPwd] = useState('');
-    const [tel, setTel] = useState();
+    const [tel, setTel] = useState('');
     let createAccount = (e) => {
         e.preventDefault();
         console.log(username);
@@ -16,8 +16,8 @@ export default () => {
             <form>
                 <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 <input type="password" id="password" value={pwd} onChange={(e) => setPwd(e.target.value)}></input>
-                <input type="number" id="tel" value={tel} onChange={(e) => setTel(e.target.value)}></input>
-                <button onClick={createAccount}>Creer un compte</button>
+                <input type="tel" id="tel" value={tel} onChange={(e) => setTel(e.target.value)}></input>
+                <button onSubmit={createAccount}>Creer un compte</button>
             </form>
         </div>       
     );
